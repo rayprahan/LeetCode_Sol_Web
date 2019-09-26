c22632a477c44a9db9f0b6fee96667835fae7b86
RicciWoo <ricciwoo@gmail.com>
2019-09-26 10:18:50 -0700

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default ({ data }) => (
+  <Layout>
+    <h1>About {data.site.siteMetadata.title}</h1>
+    <p>
+      I'm trying to post all LeetCode Solutions.
+    </p>
+    <p>by Ricky Wu</p>
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
